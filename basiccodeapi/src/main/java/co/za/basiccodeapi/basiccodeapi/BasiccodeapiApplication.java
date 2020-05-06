f5b1fc601692a10da82f62646e7845e22c867627
Sizwe Mnothoza <sizwemorrismnothoza50@gmail.com>
2020-05-07 01:02:41 +0200

    User subscription model

--- basiccodeapi/src/main/java/co/za/basiccodeapi/basiccodeapi/BasiccodeapiApplication.java
+++ basiccodeapi/src/main/java/co/za/basiccodeapi/basiccodeapi/BasiccodeapiApplication.java
@@ -2,12 +2,27 @@ package co.za.basiccodeapi.basiccodeapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BasiccodeapiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BasiccodeapiApplication.class, args);
+	}
+	
+	//cors
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**");
+			}
+		};
 	}
 
 }
---
+++ basiccodeapi/src/main/java/co/za/basiccodeapi/basiccodeapi/subscribe/User.java
@@ -0,0 +1,33 @@
+package co.za.basiccodeapi.basiccodeapi.subscribe;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+	@Id
+	private String email;
+	private boolean isActive;
+
+	public boolean isActive() {
+		return isActive;
+	}
+
+	public void setActive(boolean isActive) {
+		this.isActive = isActive;
+	}
+
+	public User() {
+	
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+}
