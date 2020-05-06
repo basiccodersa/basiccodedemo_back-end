bc9150ec31a9ad8140a78ef653ec4934e8977b06
Sizwe Mnothoza <sizwemorrismnothoza50@gmail.com>
2020-05-07 00:24:50 +0200

    first commint 001
    project setup

---
+++ basiccodeapi/src/main/java/co/za/basiccodeapi/basiccodeapi/BasiccodeapiApplication.java
@@ -0,0 +1,13 @@
+package co.za.basiccodeapi.basiccodeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasiccodeapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasiccodeapiApplication.class, args);
+	}
+
+}
